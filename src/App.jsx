5849c71859c2504fc3017b69c481ae926fb6aa44
laecio22 <laecon22@hotmail.com>
2026-01-06 22:14:57 -0300

    feat(App) importação do  componente  InputField

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import './styles/globalStyle.scss'
+import "./styles/globalStyle.scss";
 import Header from "@/components/Header/Header";
+import InputField from "@/components/InputField";
 
 function App() {
   return (
     <>
-     <Header/>
-     <h1>Hello World!</h1>
-
+      <Header />
+      <h1>Hello World!</h1>
+      {/* <InputField type='text' placeholder='Informe  o  nome'/> */}
     </>
-  )
+  );
 }
 export default App;
