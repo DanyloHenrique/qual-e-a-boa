d811343639b43f41d3fe390a79b07145a0cebb3b
laecio22 <laecon22@hotmail.com>
2026-01-07 15:45:59 -0300

    style(header.module.scss):remoção  da  borda  do  header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "./styles/globalStyle.scss";
 import Header from "@/components/Header/Header";
-import InputField from "@/components/InputField";
 
 function App() {
   return (
     <>
       <Header />
-      <h1>Hello World!</h1>
-      {/* <InputField type='text' placeholder='Informe  o  nome'/> */}
+      <h1>Hello World!</h1>   
     </>
   );
 }
