49935648556f15b36c4303d990c2599fb218e292
Danylo <danylohenrique2407@gmail.com>
2026-01-08 15:00:34 -0300

    refactor(buttonLink): update to use react icons lib

--- src/components/ButtonLink/ButtonLink.jsx
+++ src/components/ButtonLink/ButtonLink.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 import s from "./buttonLink.module.scss";
 
-function ButtonLink({ text, to, icon, isSecondaryLink }) {
+function ButtonLink({ label, to, icon: Icon, iconSize = 20, isSecondaryLink }) {
   return (
     <Link
       to={to}
@@ -11,10 +11,44 @@ function ButtonLink({ text, to, icon, isSecondaryLink }) {
         isSecondaryLink ? s.buttonLinkSecondary : ""
       }`}
     >
-      {text}
-      {icon && <img src={icon} aria-hidden />}
+      {label}
+      {Icon && <Icon size={iconSize} aria-hidden />}
     </Link>
   );
 }
 
 export default ButtonLink;
+
+/* envolver no componente BrowserRouter para testar
+
+Props:
+label: texto do link
+to: pagina de direcionamento
+icon: icone caso seja utilizado
+iconSize: tamanho do icone
+isSecondaryLink: para links secundarios (os links pretos com fundos transparente)
+
+teste:
+import { FiArrowRight } from "react-icons/fi";
+import { FaHeart } from "react-icons/fa";
+
+    <BrowserRouter>
+      <ButtonLink
+        label="Explorar"
+        to="/produtos"
+        icon={FiArrowRight}
+        iconSize={24}
+      />
+
+      <ButtonLink label="Página inicial" to="/" />
+
+      <ButtonLink label="Como funciona" to="/" isSecondaryLink />
+
+      <ButtonLink
+        label="Favoritar produto"
+        to="/"
+        icon={FaHeart}
+        isSecondaryLink
+      />
+    </BrowserRouter>
+*/
