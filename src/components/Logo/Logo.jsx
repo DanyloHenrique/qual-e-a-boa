fc5c0bbe52fe3ed89d8ed050b450e74306027a13
karoline Ernest <ernest.karoline@gmail.com>
2026-01-12 16:32:33 -0300

    refactor: trocar logo manual por imagem, removendo asset antigo e renomear o texto

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,11 +1,11 @@
 import s from './Logo.module.scss';
-import locationIcon from '../../assets/icons/location.svg';
+import logo from '../../assets/icons/logo.png'
 
-export default function Logo({ text = 'VAIBE-LOCAL', alt = 'Logo' }) {
+export default function Logo({ text = 'QUAL É A BOA?', alt = 'Logo' }) {
   return (
     <div className={s.logoContainer}>
-      <div className={s.iconWrapper}>
-        <img src={locationIcon} alt={alt} className={s.icon} />
+      <div>
+        <img src={logo} alt={alt} />
       </div>
 
       <span className={s.text}>{text}</span>
