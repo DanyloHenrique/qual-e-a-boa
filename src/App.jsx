295094ad63cc74d7c6f4247278786bb2f8a4242b
Anthony da Silveira Bugs <anthonysilveira1011@outlook.com>
2026-01-15 13:03:34 -0300

    chore: restaura App.jsx original

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import './styles/globalStyle.scss'
-import Footer from "./components/Footer/Footer";
 
 function App() {
-  return <Footer />;
-  // return <h1>Hello World!</h1>;
+  return <h1>Hello World!</h1>;
 }
 
 export default App;
