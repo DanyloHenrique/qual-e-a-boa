5b9744d6bea04a1aaf3e6120cbed0d12ad046f39
laecio22 <laecon22@hotmail.com>
2026-01-15 10:59:44 -0300

    refactor(Header):retirada da função  de handleClose  no nav,  que  tava fechando  o dropdown  no  clique  em qualquer  lugar da  tela

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import s from "./header.module.scss";
 import ButtonLink from "../ButtonLink/ButtonLink";
 import Logo from "../Logo/Logo";
-import { BrowserRouter } from "react-router-dom";
+
 import { useState } from "react";
 import { FiMenu } from "react-icons/fi";
 import { MdClose } from "react-icons/md";
@@ -10,7 +10,7 @@ const Header = () => {
   const [open, setOpen] = useState(false);
 
   const handleToggle = () => setOpen((prevOpen) => !prevOpen);
-  const handleClose = () => setOpen(false);
+  // const handleClose = () => setOpen(false);
 
   return (
     <header className={s.header}>
@@ -25,7 +25,7 @@ const Header = () => {
           {open ? <MdClose size={22} /> : <FiMenu size={22} />}
         </button>
 
-        <nav className={`${s.nav} ${open ? s.open : ""}`} onClick={handleClose}>
+        <nav className={`${s.nav} ${open ? s.open : ""}`}>
           <ButtonLink label="Eventos" to="/" isSecondaryLink />
           <ButtonLink label="Categorias" to="/" isSecondaryLink />
           <ButtonLink label="Sobre" to="/" isSecondaryLink />
