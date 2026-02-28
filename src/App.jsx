dbf57dacc85e7e0cef08c06337de84e82f39bdba
laecio22 <laecon22@hotmail.com>
2026-02-28 15:03:09 -0300

    feat(Newsletter.jsx):Criação  do  componente  Newsletter da  aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import './styles/globalStyle.scss'
+import Newsletter from "@/pages/home/components/Newsletter/Newsletter";
+
 
 function App() {
-  return <h1>Hello World!</h1>;
+  // return <h1>Hello World!</h1>;
+  return (
+    <>
+     <Newsletter/>
+    </>
+  )
 }
 
 export default App;
