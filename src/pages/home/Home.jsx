894ae4c14401af8301f5e66f5d3032f25c4b76b1
Danylo <danylohenrique2407@gmail.com>
2026-03-06 01:42:18 -0300

    feat: implement background gradient accents

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,16 @@
-import S  from './home.module.scss';
 import Newsletter from "./components/Newsletter/Newsletter";
+import S from "./home.module.scss";
 
 const Home = () => {
-  return (
-    <main>
-      <section className={S.containerNewsletter}>
-        <Newsletter />
-      </section>
-    </main>
-  );
+	return (
+		<main>
+			<section className={S.containerNewsletter}>
+				<span className={S.circle_one} />
+				<Newsletter />
+				<span className={S.circle_two} />
+			</section>
+		</main>
+	);
 };
 
 export default Home;
