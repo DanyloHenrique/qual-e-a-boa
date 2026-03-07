f6f905961b53821de9c2b5d11c81cceb156f5453
laecio22 <laecon22@hotmail.com>
2026-03-07 08:11:03 -0300

    refactor(_variables.scss):mudança na forma nomenclatura  da  cor e  retirada do  import  do  componente  Home  no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import React from "react";
-import './styles/globalStyle.scss'
-import Home from "@/pages/home/Home";
-
-
+import "./styles/globalStyle.scss";
 
 function App() {
-  // return <h1>Hello World!</h1>;
-  return (
-    <Home/>
-  )
+  return <h1>Hello World!</h1>;
 }
 
 export default App;
